d9b4014d683c8158d14e128f31ba2c6e72b4d9a0
doosemavis <moosedavis2011@gmail.com>
2021-01-14 09:38:51 -0600

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Cell extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            color: ''
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="cell">
+                
+            </div>
+        )
+    }
+}
